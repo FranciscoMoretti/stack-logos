3011a618602c1e3305e560b6f767441d42d51731
Francisco Moretti <franciscoemoretti@gmail.com>
2022-04-28 11:27:26 +0100

    craete a dummy image component

---
+++ components/LogoImage.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import React from 'react';
+
+interface LogoImageProps {
+  image: string,
+  name: string,
+}
+
+interface LogoImageState {
+}
+
+export default class LogoImage extends React.Component<LogoImageProps,LogoImageState> {
+  render() {
+    return (
+        <Image
+        src={this.props.image}
+        alt={this.props.name}
+        width={200}
+        height={200}
+      />
+    );
+  }
+}
