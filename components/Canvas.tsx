4c8307e1474cd99d3198d48440877377e4b6f27a
Francisco Moretti <franciscoemoretti@gmail.com>
2022-05-01 11:36:08 +0100

    limit width to height of canvas

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -21,7 +21,8 @@ export default class Canvas extends React.Component<CanvasProps, CanvasState>  {
       const ctx = canvas.getContext('2d');
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       
-      const size = canvas.width / this.props.imageList.length
+      const maxWidth = canvas.width / this.props.imageList.length 
+      const size = maxWidth < 300 ? maxWidth : 300; 
       for (let i = 0; i < this.props.imageList.length; i++) {
         let imageObj1 = new Image();
         imageObj1.src = this.props.imageList[i];
