b53263c75568d34453695779c17d3fe1a0b5b2c6
Francisco Moretti <franciscoemoretti@gmail.com>
2022-05-01 10:31:04 +0100

    clear canvas on update

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -11,13 +11,15 @@ export default class Canvas extends React.Component<CanvasProps, CanvasState>  {
   componentDidMount(): void {
     this.updateCanvas();
   };
-  
+
   componentDidUpdate(): void {
     this.updateCanvas();
   };
 
   updateCanvas() {
-      const ctx = this.refs.canvas.getContext('2d');
+      const canvas = this.refs.canvas
+      const ctx = canvas.getContext('2d');
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
 
       var imageObj1 = new Image();
       imageObj1.src = this.props.imageList[0];
