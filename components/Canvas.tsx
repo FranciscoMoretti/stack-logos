b07ecd410f6e85ac6dae2d9c4d02ae265cfb7c12
Francisco Moretti <franciscoemoretti@gmail.com>
2022-05-01 11:24:28 +0100

    print multiple components

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -20,12 +20,17 @@ export default class Canvas extends React.Component<CanvasProps, CanvasState>  {
       const canvas = this.refs.canvas
       const ctx = canvas.getContext('2d');
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-      var imageObj1 = new Image();
-      imageObj1.src = this.props.imageList[0];
-      imageObj1.onload = function() {
-        ctx.drawImage(imageObj1, 0, 0, 200, 200);
+      
+      const size = canvas.width / this.props.imageList.length
+      for (let i = 0; i < this.props.imageList.length; i++) {
+        var imageObj1 = new Image();
+        imageObj1.src = this.props.imageList[i];
+        imageObj1.onload = function() {
+          ctx.drawImage(imageObj1, 0 + size*i, 0, size, size);
+        }
       }
+      
+
   };
   render() {
     return (
