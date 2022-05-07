412896682bf07f679fa03584a2920379c492a86f
Francisco Moretti <franciscoemoretti@gmail.com>
2022-05-07 11:46:18 +0100

    renamed title and page name

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,16 +7,13 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Stack Logos</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+          Welcome to Stack Logos
         </h1>
         <StackEditor
         />
