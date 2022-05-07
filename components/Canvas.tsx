fd7cad957f28b464c8c8588944eb3fd890376e05
Francisco Moretti <franciscoemoretti@gmail.com>
2022-05-07 11:12:59 +0100

    bug-fix improve scaling of logos into canvas

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -5,22 +5,24 @@ import { fabric } from "fabric";
 export default function Canvas({imageList}) {
   React.useEffect(() => {
     const canvas = new fabric.Canvas("canvas");
+    const numImages = imageList.length
+    
+    const squareSize = Math.min(canvas.height, canvas.width/numImages)
 
-    const maxWidth = canvas.width / imageList.length 
-    const size = maxWidth < canvas.height ? maxWidth : canvas.height; 
+
     for (let i = 0; i < imageList.length; i++) {
       fabric.loadSVGFromURL(imageList[i], function(objects, options) {
         var obj = fabric.util.groupSVGElements(objects, options);
-        const height = obj.height * (size/obj.width)
-        obj.set({
-          left: 0 + size*i,
+        const origHeight = obj.getScaledWidth()
+        const origWidth = obj.getScaledHeight()
+        const imageScaleFactor = squareSize / Math.max(origHeight, origWidth)
+        const newobj = obj.scale(imageScaleFactor)
+
+        newobj.set({
+          left: 0 + squareSize*i,
           top: 0,
-          width: size,
-          height: height,
-          scaleX: size/obj.width,
-          scaleY: size/obj.width,
         });
-        canvas.add(obj).renderAll();
+        canvas.add(newobj).renderAll();
       });
     }
 
