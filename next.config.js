60c9f6690a7ddda1ba7fa319a98d6cb2bc7ae4ba
Francisco Moretti <franciscoemoretti@gmail.com>
2022-05-17 11:42:37 +0100

    bug-fix shared library mission on Vercel

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+// For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
+// The package "dom-to-image" has a dependency on "node-canvas"
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
+
 module.exports = {
   reactStrictMode: true,
 }
